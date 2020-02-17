07975973846b41b7a92b2a6050efd75fd29f58ca
trickyscars <mishaabesadze76@gmail.com>
2020-02-17 23:26:11 +0400

    added section shop weekly advantage__counter

--- app/js/main.js
+++ app/js/main.js
@@ -4,5 +4,12 @@ $(function() {
         rating: 3.6,
         starWidth: "12px"
     });
+    $(".weekly-star").rateYo({
+        rating: 3.6,
+        starWidth: "12px"
+    });
+
+    var mixer = mixitup('.shop__inner');
+
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,8 @@ gulp.task('script', function() {
 
             'node_modules/slick-carousel/slick/slick.js',
             'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
-            'node_modules/rateyo/src/jquery.rateyo.js'
+            'node_modules/rateyo/src/jquery.rateyo.js',
+            'node_modules/mixitup/dist/mixitup.js'
 
 
         ])

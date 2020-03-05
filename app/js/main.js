f578d2c7525630d5504db2a231a890d6ead23dec
trickyscars <mishaabesadze76@gmail.com>
2020-03-05 12:12:33 +0400

    added page new items

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,25 @@
 $(function() {
+    $(".menu__item-link").on("click", function(e) {
+        e.preventDefault();
+        $(".drop__list").toggleClass("nameClass--active");
+    });
+
+    $(".wordi").on("click", function(e) {
+        e.preventDefault();
+        $(".wordi-drop").toggleClass("nameClass--active");
+    });
+
+    $(".prod").on("click", function(e) {
+        e.preventDefault();
+        $(".prod-list").toggleClass("nameClass--active");
+    });
+
+    $(".pages__drop").on("click", function(e) {
+        e.preventDefault();
+        $(".page__wrapper").toggleClass("nameClass--active");
+    });
+
+
 
     $(".rate-star").rateYo({
         rating: 3.6,
@@ -9,7 +30,37 @@ $(function() {
         starWidth: "12px"
     });
 
+    $("#example").ionRangeSlider({
+        type: "double",
+
+        min: 0,
+        max: 330,
+        from: 30,
+        to: 300,
+        prefix: "$"
+    });
+
+    $('.icon-list').on('click', function() {
+        $('.categories__items').addClass('list');
+        $('.icon-list').addClass('active');
+        $('.icon-code').removeClass('active');
+        $('.breadcrumbs__list').addClass('grid');
+        // $('.icon-list').addClass('active');
+
+    });
+    $('.icon-code').on('click', function() {
+        $('.categories__items').removeClass('list');
+        $('.icon-code').addClass('active');
+        $('.icon-list').removeClass('active');
+        $('.breadcrumbs__list').removeClass('grid');
+
+    });
+    new WOW().init();
+
     var mixer = mixitup('.shop__inner');
+
+
+
 
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,9 @@ gulp.task('script', function() {
             'node_modules/slick-carousel/slick/slick.js',
             'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
             'node_modules/rateyo/src/jquery.rateyo.js',
-            'node_modules/mixitup/dist/mixitup.js'
+            'node_modules/mixitup/dist/mixitup.js',
+            'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+            'node_modules/wow.js/dist/wow.js'
 
 
         ])
@@ -39,7 +41,10 @@ gulp.task('style', function() {
             'node_modules/normalize.css/normalize.css',
             'node_modules/slick-carousel/slick/slick.css',
             'node_modules/magnific-popup/dist/magnific-popup.css',
-            'node_modules/rateyo/src/jquery.rateyo.css'
+            'node_modules/rateyo/src/jquery.rateyo.css',
+            'node_modules/ion-rangeslider/css/ion.rangeSlider.css',
+            'node_modules/animate.css/animate.css'
+
         ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
